3be1762018d309f7faa72cdb93bf19ea4cbb537e
Akira Kawamata <autumn@piedey.co.jp>
2019-05-22 11:24:30 +0900

    非同期にできないMainメソッドの矛盾を解決

--- Wonbe/WonbeFW/Program.cs
+++ Wonbe/WonbeFW/Program.cs
@@ -12,21 +12,34 @@ namespace WonbeFW
         [STAThread]
         static void Main(string[] args)
         {
-            Console.WriteLine($"*** Welcome, {WonbeLib.Wonbe.GetMyName()} Command Line ***");
-            var info = new LanguageBaseStartInfo();
-            info.CommandLine = Environment.CommandLine;
-            if (args.Length > 0) info.SourceCodeFileName = args[0];
-            info.RunRequest = args.Length > 0;
-            var env = new WonbeEnviroment();
-            var b = new WonbeLanguageBase();
-            b.Environment = env;
-            Console.CancelKeyPress += (sender, e) =>
+            AutoResetEvent termEvent = new AutoResetEvent(false);
+            var thread = new Thread(async () =>
             {
-                IsCanceled = true;
-                e.Cancel = true;
-            };
-            _= b.InvokeInterpreterAsync(info, () => IsCanceled, (v) => IsCanceled = v);
-            //Thread.Sleep(10000);
+                try
+                {
+                    Console.WriteLine($"*** Welcome, {WonbeLib.Wonbe.GetMyName()} Command Line ***");
+                    var info = new LanguageBaseStartInfo();
+                    info.CommandLine = Environment.CommandLine;
+                    if (args.Length > 0) info.SourceCodeFileName = args[0];
+                    info.RunRequest = args.Length > 0;
+                    var env = new WonbeEnviroment();
+                    var b = new WonbeLanguageBase();
+                    b.Environment = env;
+                    Console.CancelKeyPress += (sender, e) =>
+                    {
+                        IsCanceled = true;
+                        e.Cancel = true;
+                    };
+                    await b.InvokeInterpreterAsync(info, () => IsCanceled, (v) => IsCanceled = v);
+                    //Thread.Sleep(10000);
+                }
+                finally
+                {
+                    termEvent.Set();
+                }
+            });
+            thread.Start();
+            termEvent.WaitOne();
         }
     }
 }
